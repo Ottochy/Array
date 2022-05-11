cbc25b94ea1fc20e2694f151bdfd1a59cee577fb
Ottochy <102737499+Ottochy@users.noreply.github.com>
2022-05-11 08:33:09 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+//let notaHTML = 10
+//let notaCSS = 10
+//let notaJS - 10
+
+//let media = ((notaHTML+notaCSS+notasJS)/3)
+//console.log(media)
+
+let notas = [9, 5, 6]
+
+console.log(notas)
+console.log(notas.length)
+
+let media = (notas[0]+notas[1]+notas[2])/notas.length
+console.log(media)
---
+++ arrayADD.js
@@ -0,0 +1,6 @@
+const notas = [10, 2, 8]
+notas.push(7)
+console.log(notas)
+
+const medias = (notas[0]+notas[1]+notas[2]+notas[3])/4;
+console.log(medias)
